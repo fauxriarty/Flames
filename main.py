4c5d9d3a196ca184d414ffb53b25afdecad454dc
Chandram-Dutta <chandramdutta2004@gmail.com>
2022-11-26 23:25:12 +0530

    Adding a bit of UI magic

--- main.py
+++ main.py
@@ -1,33 +1,67 @@
-import os
+import flet as flet
+from flames import flames
 
-name1= input("Enter your name: ")
-name2= input("Enter your crush's name: ")
 
-#removing the common characters:
-s1=''.join(i for i in name1 if i not in name2 )
-s2=''.join(i for i in name2 if i not in name1)
-n=len(s1)+len(s2)
-
-def flames(n):
-    s= ['Friends', 'Lovers', 'Acquaintances', 'Marriage', 'Enemies', 'Siblings']
-
-    while len(s)>1:
-        if n%len(s)!=0:
-            t= n%len(s)
-            if t>len(s):
-                t=t%len(s) -1
-            else:
-                t=t-1
+def main(page: flet.page):
+    def on_submit(e):
+        if your_name_input.value and their_name_input.value == "":
+            your_name_input.error_text = "Enter Values"
+            their_name_input.error_text = "Enter Values"
+            page.update()
         else:
-            t = len(s)-1
-        s.remove(s[t])
-    print(s[0])
+            answer = flames(your_name_input.value, their_name_input.value)
+            relationship.value = "Your relationship with " + their_name_input.value + " can be " + answer
+            your_name_input.error_text = ""
+            their_name_input.error_text = ""
+            page.update()
+
+    page.appbar = flet.AppBar(
+        title=flet.Text("Flames 🔥"),
+        center_title=True,
+    )
+
+    your_name_input = flet.TextField(
+        label="Your Name"
+    )
+
+    their_name_input = flet.TextField(
+        label="Their Name"
+    )
+    submit_button = flet.ElevatedButton(
+        "Check",
+        on_click=on_submit
+    )
+    relationship = flet.Text(
+        text_align=flet.TextAlign.CENTER,
+        value="",
+        weight=flet.FontWeight.BOLD,
+        color=flet.colors.ORANGE,
+        size=32
+    )
+
+    column = flet.Column(
+        expand=True,
+        alignment=flet.MainAxisAlignment.CENTER,
+        horizontal_alignment=flet.CrossAxisAlignment.CENTER,
+        controls=[
+            your_name_input,
+            their_name_input,
+            submit_button,
+        ]
+    )
+
+    main_column = flet.Column(
+        alignment=flet.MainAxisAlignment.SPACE_BETWEEN,
+        horizontal_alignment=flet.CrossAxisAlignment.CENTER,
+        expand=True,
+        controls=[
+            column,
+            relationship
+        ]
+    )
+    page.add(
+        main_column
+    )
 
 
-print("Calculating...")
-os.system('cls')
-print(f"\nYour relationship with {name2} is: ")
-flames(n)
-
-
-
+flet.app(target=main)
